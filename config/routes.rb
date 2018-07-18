86dfa1f2592f428ed0f7cb65ef7631de2244fea0
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-07-18 16:28:54 +0700

    rating on product

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "remove_product/:product_id", to: "carts#remove_product", as: :remove_product
   get "update_quantity/:product_id", to: "carts#update_quantity", as: :update_quantity
   get "remove_cart", to: "carts#remove_cart", as: :remove_cart
+  post "/rate", to: "rater#create", as: :rate
 
   namespace :admin do
     resources :users, :categories, :products, :orders, :images, :product_temps
